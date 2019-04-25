41a8f5fa446a7c12ebb37a0e2f0704f0bc8f62f4
Collin May <CMay6574@student.whatcom.edu>
2019-04-25 16:57:42 -0700

    convert to AndroidX (whatever that is)

--- app/src/main/java/edu/whatcom/mywcc/ProfileActivity.java
+++ app/src/main/java/edu/whatcom/mywcc/ProfileActivity.java
@@ -1,7 +1,7 @@
 
 package edu.whatcom.mywcc;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class ProfileActivity extends AppCompatActivity {
